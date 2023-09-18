976121508f29c2467b38951bf1e9636fb4207583
unknown <csmcc2@hotmail.com>
2023-09-18 11:29:31 +0800

    bfx

--- src/mdp.test.ts
+++ src/mdp.test.ts
@@ -1,8 +1,7 @@
-import { mdpr_images } from "./index"
+import { getMdprImages } from "./index"
 
 test("get images ok", async () => {
-    const image_urls = await mdpr_images("https://mdpr.jp/cinema/3928728")
+    const image_urls = await getMdprImages("https://mdpr.jp/cinema/3928728")
     const image_len = image_urls.length
-    console.log(image_len)
     expect(image_len).toEqual(50)
 })
